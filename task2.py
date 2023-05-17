485ade3b1d815fae828f82b0cb2a21df9f530001
Kara Sawamura <sawamurakara@gmail.com>
2023-05-16 20:34:14 -0700

    Added searching for matches but error invalid salt

--- task2.py
+++ task2.py
@@ -2,6 +2,9 @@ import nltk
 nltk.download()
 from nltk.corpus import words
 words.words()
+import bcrypt
+import time
+import base64
 
 
 
@@ -35,6 +38,36 @@ def bCrypt(line):
     print(workfactor)
     print(salt)
     print(hash)
+    start = time.time()
+    for word in nltk.corpus.abc.words():
+        if len(word) >= 6 and len(word) <= 10:
+            print("type of salt before .encode: ", type(salt))
+            print("type of salt: ", type(salt.encode('ascii')))
+            print("type of word converted: ", type(word.encode('utf8')))
+            # if bcrypt.hashpw(word.encode('ascii'), salt.encode('utf8')) == hash:
+            # if bcrypt.hashpw(word.encode('ascii'), base64.b64decode(salt).encode('utf8')) == hash:
+            salt2 = salt.encode()
+            salt3 = base64.b64decode(salt2)
+            print("type of salt3: ", type(salt3))
+            if bcrypt.hashpw(word.encode('ascii'), salt3) == hash:
+                end = time.time()
+                print("nltk word: ", word)
+                print("hash: ", hash)
+                print("Start: ", start)
+                print("End: ", end)
+                print("Total Time: ", end - start)
+                return True
+    print("Returned False")
+    print("hash: ", hash)
+    print("Start: ", start)
+    print("End: ", end)
+    print("Total Time Elapsed: ", end - start)
+    return False
+    
+#  Given: salt, hashed, hashing algorithm
+# Do:
+# Go through each world in corpus and hash it with salt and see if the resulting hash matches the given hashed 
+
 
 
     print(nltk.corpus.abc.words())
